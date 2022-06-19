c1bb9ac0cc6892f93240ba25a00cd2f996707ba5
Aditya Bapat <adityawohlig@gmail.com>
2022-06-19 16:37:51 +0530

    popup err

---
+++ src/components/popups/stocks/buySell.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-dialog v-model="dialog2" width="500">
+          <v-card>
+            <v-card-title class="text-h5 grey lighten-2"> Buy </v-card-title>
+
+            <v-card-text> {{ stock.name }} : {{ stock.price }} </v-card-text>
+
+            <v-divider></v-divider>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" text @click="dialog2 = !dialog2;"> Buy </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+</template>
+
+
+<script>
+export default {
+    name: "BuySellPopup",
+    props: ['modal','stock'],
+    data() {
+        return {
+        dialog2: this.modal,
+        };
+    },
+    methods: {
+        closeDialog() {
+        this.dialog2 = !this.dialog2;
+        },
+        
+    },
+    };
+</script>
--- src/views/StockView.vue
+++ src/views/StockView.vue
@@ -13,40 +13,26 @@
 
         <v-card-subtitle> CMP: {{ stock.price }} </v-card-subtitle>
 
-        <v-card-actions>
-          <v-btn color="green" class="py-0 my-0" text @click="show = !show"> Buy </v-btn>
-          <v-btn color="orange" class="py-0 my-0" text @click="show = !show"> Sell </v-btn>
-
-          <v-spacer></v-spacer>
-
-          <v-btn icon @click="show = !show">
-            <!-- <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon> -->
-          </v-btn>
-        </v-card-actions>
-
-        <!-- <v-expand-transition>
-          <div v-show="show">
-            <v-divider></v-divider>
-
-            <v-card-text>
-              I'm a thing. But, like most politicians, he promised more than he
-              could deliver. You won't have time for sleeping, soldier, not with
-              all the bed making you'll be doing. Then we'll go with that data
-              file! Hey, you add a one and two zeros to that or we walk! You're
-              going to do his laundry? I've got to find a way to escape.
-            </v-card-text>
-          </div>
-        </v-expand-transition> -->
+        <BuySellPopup :modal="dialog" :stock="stock" />
+        <v-btn color="green" class="py-0 my-0" text @click="dialog = true">
+          Buy
+        </v-btn>
+        <v-btn color="orange" class="py-0 my-0" text @click="dialog = true">
+          Sell
+        </v-btn>
+        <v-spacer></v-spacer>
       </v-card>
     </v-row>
   </div>
 </template>
 
 <script>
+import BuySellPopup from "../components/popups/stocks/buySell";
 export default {
+    components: {BuySellPopup},
   data() {
     return {
-      show: false,
+      dialog: false,
       stocks: [
         {
           name: "Reliance",
@@ -76,6 +62,12 @@ export default {
         },
       ],
     };
+  },
+
+  methods: {
+    closeDialog() {
+      this.dialog = false;
+    },
   },
 };
 </script>
